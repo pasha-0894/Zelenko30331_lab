7126d8075d9a12c86e15d724692fe7b35e8588cd
Pavel <pavelzelenko94@gmail.com>
2025-01-02 15:11:00 +0300

    Добавьте файлы проекта.

---
+++ Zelenko30331_lab/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Zelenko30331_lab.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
